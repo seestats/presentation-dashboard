e5319617d8f90ab722880fcf2debf47a166e191c
Povilas Balciunas <balciunas90@gmail.com>
2016-03-06 11:52:59 +0200

    Format counters

--- js/seestats.js
+++ js/seestats.js
@@ -3,11 +3,19 @@
 var esUrl = 'https://es.seestats.org/see-stats-2016.03.06';
 var eventCountUrl = esUrl + '/raw/_count';
 
+function formatNumber(number) {
+  return $.number(number);
+}
+
+function setCounter(id, value) {
+  $(id).text(formatNumber(value));
+}
+
 function getAllEventsCount() {
   $.ajax({
       url: eventCountUrl
   }).done(function(data) {
-      $('#all-events').text(data.count);
+      setCounter('#all-events', data.count);
   });
 }
 
